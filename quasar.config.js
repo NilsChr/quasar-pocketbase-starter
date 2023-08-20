765b8e4e6be5b1d1d281cf4701adafe09782cc93
Nils Christian Bogen <nils.chr.bogen@gmail.com>
2023-08-20 23:35:28 +0200

    changed env setup

--- quasar.config.js
+++ quasar.config.js
@@ -48,10 +48,13 @@ module.exports = configure(function (ctx) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-build
     build: {
-      vueRouterMode: 'hash', // available values: 'hash', 'history',
+      vueRouterMode: 'history', // available values: 'hash', 'history',
       env: {
-        API: ctx.dev ? "https://blogal.pockethost.io" : "https://blogal.pockethost.io",
-        BASE_URL: ctx.dev ? "http://localhost:8080" : "https://blogal-2.onrender.com"
+        API: "https://blogal.pockethost.io",
+        BASE_URL:"http://localhost:8080" 
+
+       // API: ctx.dev ? "https://blogal.pockethost.io" : "https://blogal.pockethost.io",
+       // BASE_URL: ctx.dev ? "http://localhost:8080" : "https://blogal-2.onrender.com"
       }
 
       // transpile: false,
